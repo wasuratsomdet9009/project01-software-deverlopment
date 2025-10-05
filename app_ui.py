5e03a5da35a3e1d275e9b37d3c9bb4208be14d2d
Warattayaaa <mnwrty49@gmail.com>
2025-10-05 21:44:53 +0700

    color

--- app_ui.py
+++ app_ui.py
@@ -26,7 +26,7 @@ class LoginFrame(ttk.Frame):
         style.configure('TFrame', background="#FFB1B4")                # พื้นหลัง Frame สีชมพู
         style.configure('TLabel', background="#FFB1B4", foreground='#222')   # Label ตัวอักษรดำ พื้นชมพู
         style.configure('TButton', background="#FFE1E2", foreground='#222', font=('Segoe UI', 11, 'bold'))
-        style.map('TButton', background=[('active', "#EBEBEB"), ('!active', "#FFFFFF")])
+        style.map('TButton', background=[('active', "#D0D0D0"), ('!active', "#FFFFFF")])
         frm = ttk.Frame(self); frm.pack(expand=True, fill="both")
         ttk.Label(frm, text="เข้าสู่ระบบ", font=("Segoe UI", 14, "bold")).pack(pady=(0,10))
 
